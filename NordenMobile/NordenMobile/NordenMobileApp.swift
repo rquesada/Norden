964f820d7db2997199cc043176bddc24a2668f41
Roy Quesada <roy.quesada@gmail.com>
2025-03-03 15:18:50 -0600

    Create empty project

---
+++ NordenMobile/NordenMobile/NordenMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  NordenMobileApp.swift
+//  NordenMobile
+//
+//  Created by Roy Quesada on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NordenMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NordenMobile/NordenMobileTests/NordenMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  NordenMobileTests.swift
+//  NordenMobileTests
+//
+//  Created by Roy Quesada on 3/3/25.
+//
+
+import Testing
+@testable import NordenMobile
+
+struct NordenMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
